1ef27826a2158a4ecf1339feef271a55492ad437
Balázs <Balzs@DESKTOP-03D4AHH>
2022-04-09 10:14:08 +0200

    Add project files.

---
+++ WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.keres = new System.Windows.Forms.Button();
+            this.honnan = new System.Windows.Forms.RichTextBox();
+            this.hova = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // keres
+            // 
+            this.keres.Location = new System.Drawing.Point(692, 49);
+            this.keres.Name = "keres";
+            this.keres.Size = new System.Drawing.Size(93, 125);
+            this.keres.TabIndex = 0;
+            this.keres.Text = "Keres";
+            this.keres.UseVisualStyleBackColor = true;
+            this.keres.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // honnan
+            // 
+            this.honnan.Location = new System.Drawing.Point(228, 57);
+            this.honnan.Name = "honnan";
+            this.honnan.Size = new System.Drawing.Size(240, 39);
+            this.honnan.TabIndex = 4;
+            this.honnan.Text = "";
+            // 
+            // hova
+            // 
+            this.hova.Location = new System.Drawing.Point(228, 128);
+            this.hova.Name = "hova";
+            this.hova.Size = new System.Drawing.Size(240, 39);
+            this.hova.TabIndex = 5;
+            this.hova.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(87, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 29);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Honnan";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(94, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 29);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Hova";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(83, 238);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(702, 276);
+            this.listBox1.TabIndex = 8;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(522, 100);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(111, 24);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 189);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(840, 545);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.hova);
+            this.Controls.Add(this.honnan);
+            this.Controls.Add(this.keres);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button keres;
+        private System.Windows.Forms.RichTextBox honnan;
+        private System.Windows.Forms.RichTextBox hova;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,95 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            int traveltype;
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var date = DateTime.Now;
+            var datum = DateTime.Now.ToString("yyyy-MM-dd");
+            int ora = date.Hour;
+           int perc = date.Minute;
+            listBox1.Items.Clear();
+            int traveltype=0;
+            if (comboBox1.SelectedItem == "Busz")
+            {
+                traveltype = 1;
+            }
+            if (comboBox1.SelectedItem == "Vonat")
+            {
+                traveltype = 4;
+            }
+            if (comboBox1.SelectedItem == "Mindkettő")
+            {
+                traveltype = 0;
+            }
+            try
+            {
+                string webAddr = "https://menetrendek.hu/menetrend/interface/index.php";
+
+                var httpWebRequest = WebRequest.CreateHttp(webAddr);
+                httpWebRequest.ContentType = "application/json; charset=utf-8";
+                httpWebRequest.Method = "POST";
+
+                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                {
+
+                    string json = "{ \"func\" : \"getRoutes\", \"params\" :{\"naptipus\":\"0\",\"datum\":\""+datum+"\",\"honnan\":\"" + honnan.Text + "\",\"hova\":\"" + hova.Text + "\",\"hour\":\"0\",\"min\":\"0\",\"preferencia\":\""+traveltype+ "\",\"hour\":\"" + ora + "\",\"minutes\":\"" + perc + "\"}}";
+                    streamWriter.Write(json);
+                    streamWriter.Flush();
+
+                }
+
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+
+                    var responseText = streamReader.ReadToEnd();
+                    //   Console.WriteLine(responseText);
+                    var data = (JObject)JsonConvert.DeserializeObject(responseText);
+
+
+                    for (int i = 1; i < responseText.Length; i++)
+                    {
+                        var indulasihely = data["results"]["talalatok"][$"{i}"]["indulasi_hely"];
+                        var erkezesihely = data["results"]["talalatok"][$"{i}"]["erkezesi_hely"];
+                        var erkezesiido = data["results"]["talalatok"][$"{i}"]["indulasi_ido"];
+                        var indulasiido = data["results"]["talalatok"][$"{i}"]["erkezesi_ido"];
+                        listBox1.Items.Add ($"{indulasihely} {indulasiido} - {erkezesihely} {erkezesiido}");
+
+
+
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+       
+
+            comboBox1.Items.Add("Busz");
+
+            comboBox1.Items.Add("Vonat");
+  
+            comboBox1.Items.Add("Mindkettő");
+
+        }
+    }
+}
